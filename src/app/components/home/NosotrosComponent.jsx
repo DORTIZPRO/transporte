766e14580a4d405c589286def934b7b565712102
Diego Ortiz <diego.ortiz@cummins.cl>
2023-12-28 12:43:02 -0300

    cambios en "nosotrs"

--- src/app/components/home/NosotrosComponent.jsx
+++ src/app/components/home/NosotrosComponent.jsx
@@ -13,7 +13,7 @@ const NosotrosComponent = () => {
             </div>
             <div class="md:7/12 lg:w-6/12 border rounded-xl border-blue-600 p-4">
               <h2 class="text-2xl text-blue-600 font-bold md:text-4xl ">
-                Bienvenido a Transportes DOM
+                Bienvenidos a Transportes DOM
               </h2>
               <p class="mt-6 text-gray-800">
                 En Transportes DOM, nos enorgullece ofrecer soluciones de
@@ -23,14 +23,71 @@ const NosotrosComponent = () => {
                 tus procesos de traslado, brindándote tranquilidad y confianza
                 en cada servicio.
               </p>
-              <h4 class="text-2xl text-blue-600 font-bold md:text-4xl ">
+              <br />
+              <h6 class="text-2xl text-blue-600 font-bold md:text-4xl ">
                 Nuestra Misión:
-              </h4>
+              </h6>
               <p class="mt-4 text-gray-800">
                 Facilitar tus mudanzas y fletes, brindando servicios de calidad
                 que superen tus expectativas. Nos esforzamos por ser tu socio
                 confiable en el transporte, proporcionando soluciones adaptadas
                 a tus necesidades específicas.
+              </p>
+              <br />
+              <h6 class="text-2xl text-blue-600 font-bold md:text-4xl ">
+                Nuestros Servicios:
+              </h6>
+              <br />
+              <ol start="2">
+                <li>
+                  <strong>Mudanzas Personalizadas:</strong> Diseñamos mudanzas a
+                  medida, asegurándonos de que cada paso del proceso se adapte a
+                  tus requisitos únicos.
+                </li>
+                <br />
+                <li>
+                  <strong>Fletes Eficientes:</strong> Ofrecemos servicios de
+                  fletes rápidos y seguros para satisfacer las demandas de tu
+                  empresa o negocio.
+                </li>
+                <br />
+                <li>
+                  <strong>Cobertura Regional:</strong> Ya sea que te mudes
+                  dentro o fuera de la Región Metropolitana, contamos con una
+                  red de transporte que abarca diversos destinos para tu
+                  comodidad.
+                </li>
+              </ol>
+              <br />
+              <h6 class="text-2xl text-blue-600 font-bold md:text-4xl ">
+                Por qué Elegirnos:
+              </h6>
+              <br />
+              <ol start="2">
+                <li>
+                  <strong>Experiencia Confiable:</strong> Con años de
+                  experiencia en el sector, hemos perfeccionado nuestros
+                  servicios para garantizar la satisfacción del cliente.
+                </li>
+                <br />
+                <li>
+                  <strong>Equipo Profesional:</strong> Nuestro equipo está
+                  formado por profesionales comprometidos que se esfuerzan por
+                  brindar un servicio de calidad en cada proyecto.
+                </li>
+                <br />
+                <li>
+                  <strong>Compromiso con la Excelencia:</strong> Nos esforzamos
+                  por superar tus expectativas, ofreciendo servicios de
+                  transporte que se destacan por su eficiencia y atención al
+                  detalle.
+                </li>
+              </ol>
+              <br />
+              <p>
+                Confía en Transportes DOM para hacer que tus mudanzas y fletes
+                sean simples, seguros y sin complicaciones. Estamos aquí para
+                llevarte a donde necesitas estar. :)
               </p>
             </div>
           </div>
